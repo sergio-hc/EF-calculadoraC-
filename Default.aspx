13e4ac10b2395f20852722c1cfb5a5e4c7394aa4
unknown <sergio.luis.huanca@gmail.com>
2021-12-15 07:10:59 -0400

    primera version

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        CALCULADORA C#</div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label1" runat="server" Text="Resultado"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-bottom: 0px" Text="SUMAR" Width="174px" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="RESTAR" Width="155px" />
+        </p>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="MULTIPLICAR" Width="171px" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="DIVIDIR" Width="160px" />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r;
+
+        ClassLibrary1.Class1 c1 = new ClassLibrary1.Class1();
+        r = c1.suma(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r;
+
+        ClassLibrary1.Class2 c2 = new ClassLibrary1.Class2();
+        r = c2.resta(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r;
+
+        ClassLibrary1.Class3 c3 = new ClassLibrary1.Class3();
+        r = c3.multipli(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r;
+
+        ClassLibrary1.Class4 c4 = new ClassLibrary1.Class4();
+        r = c4.div(a, b);
+        TextBox3.Text = r.ToString();
+    }
+}
